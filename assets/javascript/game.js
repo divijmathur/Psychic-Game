d48c303700ac5cad6aaf560781d9ab267d527372
Mona Lisa <divijmathur@gmail.com>
2019-06-29 18:36:22 -0700

    fixed styling changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,47 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var lettersGuessed = [];
-var computerGuess = [];
+var letterUser = [];
+var eachofLetters = null;
 
-window.onload = function(event) {
-    var computer = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-    computerGuess.push(computer);
-    console.log(computerGuess[0]);
-    
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+function farUserGuesses() {
+	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
+}
+
+countGuessesLeft();
+
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
 document.onkeyup = function(event) {
-    var playerGuess = event.key;
-    lettersGuessed.push(playerGuess);
-    console.log(computerGuess[0]);
+	guessesLeft--;
 
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-if((playerGuess == computerGuess[0]) && (guessesLeft > 0)) {
-    wins++;
-    guessesLeft = 9;
-    lettersGuessed.length=0;
-    computerGuess.length=0;
-    var computer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    computerGuess.push(computer);
-    console.log(computerGuess[0]);
-}
+	letterUser.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
 
-else if((playerGuess != computerGuess) && (guessesLeft > 0)) {
-    guessesLeft -= 1;
-}
-
-else {
-    losses++;
-    guessesLeft = 9;
-    lettersGuessed.length=0;
-    computerGuess.length=0;
-    var computer = computerChoices[Math.floor(Math.random()* computerChoices.length)];
-    computerGuess.push(computer);
-    console.log(computerGuess[0]);
-}
-
-var html = "<p>Guess what letter I'm thinking of </p>"
-            "<p>Wins: " + wins + "</p>" +
-            "<p>Losses: " + losses + "<p>" +
-            "<p>Guesses Left: " + guessesLeft + "</p>" +
-            "<p>Your guesses so far: " + lettersGuessed + "</p>";
-
-document.querySelector("#game").innerHTML = html;
-}
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
+  };
